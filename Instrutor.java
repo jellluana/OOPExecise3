a77eab52422066428f18c4253be9f9b2320dbc20
jellluana <116643108+jellluana@users.noreply.github.com>
2023-01-19 12:55:04 +0800

    Create Instrutor.java

---
+++ Instrutor.java
@@ -0,0 +1,23 @@
+public class Instructor
+{
+	public static void main(String [] args){
+		Student student;
+		StudentCard card1, card2;
+	} 
+	Instructor = new Student();
+	student.setName("Jeah Agustin");
+	student.setEmail("jeahagustin@gmail.com");
+	
+	card1 = new Student();
+	card1.setOwner(Student);
+	card1.checkOut(3);
+	
+	card2 = new StudentCard();
+	card2.setOwner(Student);
+	
+	System.out.println(Card1 Info:);
+	System.out.println(card1.toString() + "\n");
+	System.out.println(Card2 Info:);
+	System.out.println(card2.toString() + "\");
+	}
+}
